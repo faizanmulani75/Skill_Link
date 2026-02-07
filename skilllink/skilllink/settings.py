2c36ecdf09b172808f1422f5be5731b4b2f2cd26
Faizan Mulani <faizanmulani1810@gmail.com>
2026-02-07 16:59:31 +0530

    Fix 24

--- skilllink/skilllink/settings.py
+++ skilllink/skilllink/settings.py
@@ -276,7 +276,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='skilllinproj@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='bckqhhyppwpyzagb')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='pgzpxzrtxqsvmpdw')
 
 
 
