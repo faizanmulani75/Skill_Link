f40d2b21f871fbbc47063ab0df540351abd9d72a
Faizan Mulani <faizanmulani1810@gmail.com>
2026-02-07 17:34:01 +0530

    Fix 27

--- skilllink/mettings/routing.py
+++ skilllink/mettings/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/user/$', consumers.UserConsumer.as_asgi()),
-    re_path(r'ws/meet/(?P<booking_id>\d+)/$', consumers.MeetingConsumer.as_asgi()),
+    re_path(r'^ws/user/$', consumers.UserConsumer.as_asgi()),
+    re_path(r'^ws/meet/(?P<booking_id>\d+)/$', consumers.MeetingConsumer.as_asgi()),
 ]
