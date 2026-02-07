b643a70170aba8aeea3c7cd894367352d49fd5b7
Faizan Mulani <faizanmulani1810@gmail.com>
2026-02-07 19:25:51 +0530

    Fix 32

--- skilllink/skilllink/settings.py
+++ skilllink/skilllink/settings.py
@@ -275,8 +275,9 @@ ZOOM_EMAIL = "yashmadane06@gmail.com"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='skilllinproj@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='pgzpxzrtxqsvmpdw')
 
